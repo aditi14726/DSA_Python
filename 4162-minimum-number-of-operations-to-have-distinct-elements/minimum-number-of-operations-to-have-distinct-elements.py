bfdc1172ce1a5bcf5ede744c23ec2ca1d5170493
aditi14726 <kaditi876@gmail.com>
2026-02-18 22:55:50 +0530

    Time: 11 ms (92.52%) | Memory: 32.1 MB (56.37%) - LeetSync

---
+++ 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        seen = set()
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] in seen:
+                return (i + 3) // 3
+            seen.add(nums[i])
+        return 0   
